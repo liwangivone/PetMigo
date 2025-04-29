39e30528208d28aa79914b736867b3b09081879d
liwangivone <liwangivone@gmail.com>
2025-04-30 01:39:58 +0800

    kerangkaa

---
+++ frontend/lib/views/pages/pages.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
+part 'splash_screen.dart';
+part 'onboarding_pages.dart';
+part 'register_page.dart';
+part 'login_page.dart';
+part 'tnc_page.dart';
